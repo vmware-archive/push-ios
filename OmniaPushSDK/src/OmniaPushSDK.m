750b501a4e52111c0a89008f1bf7ffa9ffe3f779
Rob Szumlakowski <robsz@pivotallabs.com>
2013-12-31 13:14:13 -0600

    @xtreme-rob-szumlakowski - unit test for initializing library with front end singleton [#61068618]

--- OmniaPushSDK/src/OmniaPushSDK.m
+++ OmniaPushSDK/src/OmniaPushSDK.m
@@ -13,6 +13,9 @@
 
 static OmniaPushSDK* sharedInstance = nil;
 static dispatch_once_t once_token = 0;
+static NSObject<OmniaPushAPNSRegistrationRequest> *registrationRequest;
+static NSProxy<OmniaPushAppDelegateProxy> *appDelegateProxy;
+static UIApplication *application;
 
 @interface OmniaPushSDK ()
 
@@ -31,20 +34,57 @@ static dispatch_once_t once_token = 0;
     return sharedInstance;
 }
 
-+ (void) setSharedInstance:(OmniaPushSDK*)newSharedInstance {
-    sharedInstance = newSharedInstance;
-    once_token = 0;
-}
-
 - (instancetype) initWithRemoteNotificationTypes:(UIRemoteNotificationType)remoteNotificationTypes {
     self = [super init];
     if (self) {
-        NSObject<OmniaPushAPNSRegistrationRequest> *registrationRequest = [[OmniaPushAPNSRegistrationRequestImpl alloc] init];
-        NSProxy<OmniaPushAppDelegateProxy> *appDelegateProxy = [[OmniaPushAppDelegateProxyImpl alloc] initWithAppDelegate:[UIApplication sharedApplication].delegate registrationRequest:registrationRequest];
+        [OmniaPushSDK setupRegistrationRequest:nil];
+        [OmniaPushSDK setupApplication:nil];
+        [OmniaPushSDK setupAppDelegateProxy:nil];
         self.sdkInstance = [[OmniaPushSDKInstance alloc] initWithApplication:[UIApplication sharedApplication] registrationRequest:registrationRequest appDelegateProxy:appDelegateProxy];
         [self.sdkInstance registerForRemoteNotificationTypes:remoteNotificationTypes];
     }
     return self;
+}
+
+#pragma mark - Unit test helpers
+
+// Used by unit tests to provide a fake singleton or to reset this singleton for following tests
++ (void) setSharedInstance:(OmniaPushSDK*)newSharedInstance {
+    application = nil;
+    appDelegateProxy = nil;
+    registrationRequest = nil;
+    sharedInstance = newSharedInstance;
+    once_token = 0;
+}
+
+// Used by unit tests to provide fake registration request objects
++ (void) setupRegistrationRequest:(NSObject<OmniaPushAPNSRegistrationRequest>*)testRegistrationRequest {
+    if (registrationRequest) return;
+    if (testRegistrationRequest == nil) {
+        registrationRequest = [[OmniaPushAPNSRegistrationRequestImpl alloc] init];
+    } else {
+        registrationRequest = testRegistrationRequest;
+    }
+}
+
+// Used by unit tests to provide fake application objects
++ (void) setupApplication:(UIApplication*)testApplication {
+    if (application) return;
+    if (testApplication == nil) {
+        application = [UIApplication sharedApplication];
+    } else {
+        application = testApplication;
+    }
+}
+
+// Used by unit tests to provide fake app delegate proxy objects
++ (void) setupAppDelegateProxy:(NSProxy<OmniaPushAppDelegateProxy>*)testAppDelegateProxy {
+    if (appDelegateProxy) return;
+    if (testAppDelegateProxy == nil) {
+        appDelegateProxy = [[OmniaPushAppDelegateProxyImpl alloc] initWithAppDelegate:application.delegate registrationRequest:registrationRequest];
+    } else {
+        appDelegateProxy = testAppDelegateProxy;
+    }
 }
 
 @end
--- Specs/src/OmniaPushSDKSpec.mm
+++ Specs/src/OmniaPushSDKSpec.mm
@@ -1,30 +1,85 @@
 #import "OmniaPushSDK.h"
+#import "OmniaPushAPNSRegistrationRequest.h"
+#import "OmniaPushAppDelegateProxy.h"
+#import "OmniaPushAppDelegateProxyImpl.h"
+
+#define TEST_NOTIFICATION_TYPE UIRemoteNotificationTypeBadge
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
+
+void setFakeRegistrationRequest(NSObject<OmniaPushAPNSRegistrationRequest> *registrationRequest) {
+    SEL setupRegistrationRequestSelector = sel_registerName("setupRegistrationRequest:");
+    [OmniaPushSDK performSelector:setupRegistrationRequestSelector withObject:registrationRequest];
+}
+
+void setFakeApplication(UIApplication *application) {
+    SEL setupApplicationSelector = sel_registerName("setupApplication:");
+    [OmniaPushSDK performSelector:setupApplicationSelector withObject:application];
+}
+
+void setFakeAppDelegateProxy(NSProxy<OmniaPushAppDelegateProxy> *appDelegateProxy) {
+    SEL setupAppDelegateProxySelector = sel_registerName("setupAppDelegateProxy:");
+    [OmniaPushSDK performSelector:setupAppDelegateProxySelector withObject:appDelegateProxy];
+}
 
 SPEC_BEGIN(OmniaPushSDKSpec)
 
 describe(@"OmniaPushSDK", ^{
 
     __block OmniaPushSDK *sdk;
- 
-    beforeEach(^{
+    __block UIApplication *application;
+    __block id<UIApplicationDelegate> appDelegate;
+    __block id<OmniaPushAPNSRegistrationRequest> registrationRequest;
+    __block OmniaPushAppDelegateProxyImpl *appDelegateProxy;
 
+    __block NSData *deviceToken;
+    __block id<UIApplicationDelegate> previousAppDelegate;
+
+    beforeEach(^{
+        application = [UIApplication sharedApplication];
+        appDelegate = fake_for(@protocol(UIApplicationDelegate));
+        registrationRequest = fake_for(@protocol(OmniaPushAPNSRegistrationRequest));
+        appDelegateProxy = [[OmniaPushAppDelegateProxyImpl alloc] initWithAppDelegate:appDelegate registrationRequest:registrationRequest];
+        
+        previousAppDelegate = application.delegate;
+        deviceToken = [@"TEST DEVICE TOKEN" dataUsingEncoding:NSUTF8StringEncoding];
     });
     
     describe(@"if you try to register", ^{
 
         it(@"it should succeed", ^{
+            registrationRequest stub_method("registerForRemoteNotificationTypes:").with(TEST_NOTIFICATION_TYPE).and_do(^(NSInvocation*) {
+                [appDelegateProxy application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+            });
+            appDelegate stub_method("application:didRegisterForRemoteNotificationsWithDeviceToken:").with(application, deviceToken);
+            setFakeRegistrationRequest(registrationRequest);
+            setFakeApplication(application);
+            setFakeAppDelegateProxy(appDelegateProxy);
+            
             sdk = [OmniaPushSDK registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge];
-            sdk should_not be_nil;
+            
+            
+            registrationRequest should have_received("registerForRemoteNotificationTypes:");
+            appDelegate should have_received("application:didRegisterForRemoteNotificationsWithDeviceToken:");
+            previousAppDelegate should be_same_instance_as(application.delegate);
         });
         
     });
     
-    afterEach(^{
+    beforeEach(^{
         // TODO - wait for any active intialization to complete before ending test
 
+        // The OmniaPushSDK class is a singleton so we need to reset it between each test.
+        // Note that the setSharedInstance: class method is only used by tests and is hidden.
+        SEL setSharedInstanceSelector = sel_registerName("setSharedInstance:");
+        [OmniaPushSDK performSelector:setSharedInstanceSelector withObject:nil];
+        sdk = nil;
+    });
+    
+    afterEach(^{
+        // TODO - wait for any active intialization to complete before ending test
+        
         // The OmniaPushSDK class is a singleton so we need to reset it between each test.
         // Note that the setSharedInstance: class method is only used by tests and is hidden.
         SEL setSharedInstanceSelector = sel_registerName("setSharedInstance:");
