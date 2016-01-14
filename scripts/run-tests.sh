866d4322f1b19839f2a0f37c77b814506f6c3253
Ergin Babani <ebabani@pivotal.io>
2016-01-14 16:15:44 -0500

    ErginB/RobS [#111425942] Added default values for the variables the run-tests script uses
    Signed-off-by: Rob Szumlakowski <rszumlakowski@pivotal.io>

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,5 +6,5 @@ set -x
 xcodebuild \
   -workspace PCFPush.xcworkspace \
   -scheme "ALL PCFPushSpecs" \
-  -destination platform='iOS Simulator',name="${XCODE_SIMULATOR_NAME}",OS="${XCODE_OS_VERSION}" \
+  -destination platform='iOS Simulator',name="${XCODE_SIMULATOR_NAME-iPhone 6}",OS="${XCODE_OS_VERSION-8.4}" \
   clean build test
